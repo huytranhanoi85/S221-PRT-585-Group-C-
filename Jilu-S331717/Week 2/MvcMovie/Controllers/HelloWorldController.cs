c62adc72d4c580a86492c7d969be15027a8a6240
Jilu <s331717@students.cdu.edu.au>
2021-08-25 12:42:50 +0930

    MVCMOVIES

---
+++ Jilu-S331717/Week 2/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return ("Hello " + name + ", ID: " + 1);
+        }
+    }
+}
