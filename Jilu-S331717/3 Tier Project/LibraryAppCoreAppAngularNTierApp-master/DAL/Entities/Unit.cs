b8ac858b62eec560be70ef6f3e5939262abef18a
Jilu <s331717@students.cdu.edu.au>
2021-09-27 23:15:44 +0930

    DAL

---
+++ Jilu-S331717/3 Tier Project/LibraryAppCoreAppAngularNTierApp-master/DAL/Entities/Unit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Unit
+    {
+        public Int64 UnitID { get; set; } //(PK)
+        public String Unit_Name { get; set; }
+        public Int64 Unit_Credit { get; set; }
+    }
+}
---
+++ Jilu-S331717/3 Tier Project/LibraryAppCoreAppAngularNTierApp-master/DAL/Migrations/20210927125846_UnitEntity.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class UnitEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Units",
+                columns: table => new
+                {
+                    UnitID = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Unit_Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Unit_Credit = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Units", x => x.UnitID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Units");
+        }
+    }
+}
